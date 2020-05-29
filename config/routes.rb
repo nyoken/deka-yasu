4c523ce6efb4c8b79dc331b984e78e8874284edd
nyoken <dk525p.ss16@gmail.com>
2020-05-30 01:01:21 +0900

    ルーティング設定とログインページのデザイン作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    post 'login', to: 'devise/sessions#create'
+    delete 'signout', to: 'devise/sessions#destroy'
+  end
   resources :shops, only: %i(index show)
 end
