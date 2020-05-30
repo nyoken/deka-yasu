85af5b7c9e3959e2e8ce4359669fab5b18f7a0a7
nyoken <dk525p.ss16@gmail.com>
2020-05-30 19:41:12 +0900

    会員登録ページのデザイン作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     post 'login', to: 'devise/sessions#create'
-    delete 'signout', to: 'devise/sessions#destroy'
+    delete 'logout', to: 'devise/sessions#destroy'
+    get 'register', to: 'devise/registrations#new'
+    post 'register', to: 'devise/registrations#create'
   end
   resources :shops, only: %i(index show)
 end
