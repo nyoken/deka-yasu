1e2051d53b77098e40074a98a6b205d0f05c298b
nyoken <dk525p.ss16@gmail.com>
2020-06-13 03:04:09 +0900

    問い合わせページのコントローラと大まかなView設定

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,13 @@
+class ContactController < ApplicationController
+  def new
+  end
+
+  def confirm
+  end
+
+  def create
+  end
+
+  def complete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations',
-                                    sessions: 'users/sessions' }
   root 'home#index'
   get 'service', to: 'static#service'
+  get 'contact', to: 'contact#new'
+  post 'contact/confirm', to: 'contact#confirm'
+  get 'contact/complete', to: 'contact#complete'
+  post 'contact/complete', to: 'contact#create', as: 'create_contact'
+
+  get 'contact/new'
+  get 'contact/confirm'
+  get 'contact/create'
+  get 'contact/complete'
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    sessions: 'users/sessions' }
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     post 'login', to: 'devise/sessions#create'
