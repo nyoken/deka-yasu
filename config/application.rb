793270d759eaaaf7c2b2a6debf6f5e661d909676
nyoken <dk525p.ss16@gmail.com>
2020-07-06 23:42:22 +0900

    heroku環境でメール送信ができるように設定

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module PayTsuka
       routing_specs: false
     end
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
