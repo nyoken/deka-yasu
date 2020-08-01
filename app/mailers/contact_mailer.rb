000b17601c6b928e4c2473ddb85114b3be7fcd98
nyoken <dk525p.ss16@gmail.com>
2020-08-01 19:10:07 +0900

    問い合わせ時の管理者向け送信先メールアドレス設定を修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,6 +6,6 @@ class ContactMailer < ApplicationMailer
 
   def send_email_to_admin(contact)
     @contact = contact
-    mail(to: "#{ENV['MAIL_ADDRESS']}@gmail.com", subject: "新規お問い合わせ")
+    mail(to: ENV['MAIL_ADDRESS'], subject: "新規お問い合わせ")
   end
 end
