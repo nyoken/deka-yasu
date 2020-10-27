16377140b2392c0848f4277f74d618c15c7264fd
nyoken <dk525p.ss16@gmail.com>
2020-10-28 05:03:33 +0900

    管理者ユーザー定義用のadminカラムを追加し、管理者のみ口コミを削除できるように設定

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,24 @@
 class ReviewsController < ApplicationController
+  before_action :admin_user, only: :destroy
+
   def create
     @review = Review.new(review_params)
     @review.save
     redirect_back(fallback_location: root_path)
   end
 
+  def destroy
+    Review.find(params[:id]).destroy
+    flash[:success] = "キープリストから削除しました"
+    redirect_back(fallback_location: root_path)
+  end
+
   private
   def review_params
     params.require(:review).permit(:shop_id, :user_id, :body)
+  end
+  
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
 end
