0fd311344c92240a6defa78054b21cbad313d8df
nyoken <dk525p.ss16@gmail.com>
2020-12-06 15:18:06 +0000

    URIのエンコード時に、addressableをrequireさせる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'addressable/uri'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
