589f0b748a55d0de1e5b72083bcadcedc1680557
nyoken <dk525p.ss16@gmail.com>
2020-12-08 01:15:04 +0900

    spec/supportのファイルが返される順序を保証するためにsortを追加し、Sort files before requiring them.を解消

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,7 +24,7 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # spec/supportに、RSpec関連の設定ファイルを配置できるように設定。
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
