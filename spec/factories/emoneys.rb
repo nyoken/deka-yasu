d177bbcffd64751483fc3fcd0ddbb16c98b1987d
nyoken <dk525p.ss16@gmail.com>
2020-12-12 00:54:57 +0900

    Model Specを追加

--- spec/factories/emoneys.rb
+++ spec/factories/emoneys.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :emoney do
-    
+    sequence(:name) { |n| "emoney#{n}" }
+    category { 'category' }
+    image { 'image' }
+    link { 'link' } # null許可
+    description { 'description' }
   end
 end
--- spec/models/emoney_spec.rb
+++ spec/models/emoney_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Emoney, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:emoney) { build(:emoney) }
+
+  describe 'バリデーションの確認' do
+    it '全カラムに値が設定されていればOK' do
+      expect(emoney.valid?).to eq(true)
+    end
+
+    it '名前が空だとNG' do
+      emoney.name = nil
+      expect(emoney.valid?).to eq(false)
+    end
+
+    it 'カテゴリーが空だとNG' do
+      emoney.category = nil
+      expect(emoney.valid?).to eq(false)
+    end
+
+    it 'イメージが空だとNG' do
+      emoney.image = nil
+      expect(emoney.valid?).to eq(false)
+    end
+
+    it '概要が空だとNG' do
+      emoney.description = nil
+      expect(emoney.valid?).to eq(false)
+    end
+
+    it 'リンクが空でもOK' do
+      emoney.link = nil
+      expect(emoney.valid?).to eq(true)
+    end
+  end
 end
